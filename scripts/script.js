651eddb600123613ca5bf5f447be61d71a06af4d
Gaseines <gaseinesdzn@hotmail.com>
2023-10-18 16:07:56 -0300

    inicio da segunda parte

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,27 @@ let x = document.querySelector('.xx')
 
 
 
+function MudouTamanho(){
+    
+if(window.innerWidth >= 986){
+    burguer.style.display = 'none'
+    x.style.display = 'none'
+    img.style.opacity = 1
+    list.style.opacity = 1
+
+}else{
+    burguer.style.display = 'flex'
+    img.style.opacity = 0
+    list.style.opacity = 0
+}
+
+if(burguer.style.display == 'flex'){
+    x.style.display = 'none'
+}
+
+}
+
+
 const dropdown = () =>{
     if(img.style.opacity == 0){
         img.style.opacity = 1
